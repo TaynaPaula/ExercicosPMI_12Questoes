f0ebae460654c2863c4457babe7a1a236843dda8
TaynaPaula <taynapaulacruz@gmail.com>
2025-09-24 21:22:38 -0300

    Add files via upload

---
+++ 12_Questões/E1.py
@@ -0,0 +1,14 @@
+#Crie um programa que valide um número de telefone brasileiro e formate-o
+#corretamente usando try/except
+
+try:
+    a=input("Digite o numero de tellefone com prefixo e DDD:")
+    prefixo=a[0:2]
+    if prefixo =="55" and len(a)==13:
+        print(f"Número de telefone {a} é válido e é brasileiro")
+    elif prefixo !="55" and len(a)==13:
+        print(f"Número de telefone {a} é válido mas não é brasileiro")
+    else:
+        print(f"Número de telefone {a} não é válido")
+except ValueError:
+    print("Informe os valores corretamente.")
